08ce691b51980e8259a1f8522ae70c87789e458e
root <root@Elyes.>
2024-10-31 13:00:03 +0100

    aaa

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,6 +8,7 @@ void times_table(void);
 int print_sign(int n);
 void jack_bauer(void);
 int _abs(int n);
+void print_to_98(int n);
 int print_last_digit(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
