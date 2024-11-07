e167d73325dc0b2f40bdb70c58915d9cdba5483f
root <root@Elyes.>
2024-11-07 11:22:28 +0100

    aaa

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -4,7 +4,6 @@
  * _pow_recursion - Returns the value of x raised to the power of y.
  * @x: The base number.
  * @y: The exponent.
- * Return: The result of x raised to the power of y, or -1 if y is lower than 0.
  */
 int _pow_recursion(int x, int y)
 {
