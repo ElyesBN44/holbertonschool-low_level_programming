bd0f9e2083be0e50c39192bae0007ca6e89b8b25
root <root@Elyes.>
2024-11-15 10:43:26 +0100

    aaa

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+int size;
+int *array;
+int i;
+if (min > max)
+{
+return (NULL);
+}
+size = max - min + 1;
+array = malloc(size *sizeof(int));
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+array[i] = min + i;
+}
+return (array);
+}
