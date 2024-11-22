3ae6c0ef0c71b6fd921cd99110f78b25fb109dcd
root <root@Elyes.>
2024-11-22 12:32:54 +0100

    aaa

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,35 +9,38 @@
  */
 void print_all(const char * const format, ...)
 {
-va_list ap;
-int i = 0;
-char *s;
-va_start(ap, format);
-while (format[i])
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(ap, int));
-break;
-case 'i':
-printf("%d", va_arg(ap, int));
-break;
-case 'f':
-printf("%f", va_arg(ap, double));
-break;
-case 's':
-s = va_arg(ap, char *);
-if (s == NULL)
-printf("(nil)");
-else
-printf("%s", s);
-break;
-}
-i++;
-if (format[i])
-printf(", ");
-}
-va_end(ap);
-printf("\n");
+	va_list ap;
+	unsigned int i = 0;
+
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			{
+				char *s = va_arg(ap, char *);
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				break;
+			}
+		default:
+			break;
+		}
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
 }
